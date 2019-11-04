10c059c2811cc545f6e99aba71ac6ad83a59dfe0
GabrielUnal <gebejaranod@unal.edu.co>
2019-11-03 22:24:32 -0500

    first

--- src/login/resolvers.js
+++ src/login/resolvers.js
@@ -3,24 +3,38 @@ import { url, port, entryPoint } from './server';
 var jwt = require('jsonwebtoken')
 const URL = `http://${url}:${port}/${entryPoint}`;
 
+const crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
 
-
+function decrypt(text){
+	var decipher = crypto.createDecipher(algorithm,password)
+	var dec = decipher.update(text,'hex','utf8')
+	dec += decipher.final('utf8');
+	return dec;
+}
 const resolvers = {
 	Query: {
 	},
 	Mutation: {
 		login: async (_, { credentials }) =>{
-			let res = await	generalRequest(`${URL}`, 'POST', credentials)
-			if(res){
-				if(res === 'LDAPException found'){
-					return res
+			let pass = decrypt(credentials.password);
+			credentials.password = pass;
+			try{
+				let res = await	generalRequest(`${URL}`, 'POST', credentials)
+				if(res){
+					if (res === 'LDAPException found'){
+						return res
+					}else{
+						var token = await jwt.sign({username: res}, 'Secret Password', {expiresIn: 60 * 60 * 24})
+						return token
+					}				
 				}else{
-					var token = await jwt.sign({username: res}, 'Secret Password', {expiresIn: 60 * 60 * 24})
-					return token
-				}				
-			}else{
-				return -1
-			}
+					return -1
+				}
+			}catch(err){
+				console.log(err)
+			}			
 		}			
 	}
 };
